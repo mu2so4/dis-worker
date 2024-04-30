a1c76344ee773a8064d0957a599a132f48facbe6
mu2so4 <b4.orangutang@gmail.com>
2024-04-30 14:46:26 +0700

    Fix worker-sided farewell issue

---
+++ src/main/java/ru/nsu/ccfit/muratov/distributed/crack/worker/dto/WorkerIdDto.java
@@ -0,0 +1,8 @@
+package ru.nsu.ccfit.muratov.distributed.crack.worker.dto;
+
+import lombok.Data;
+
+@Data
+public class WorkerIdDto {
+    private String id;
+}
--- src/main/java/ru/nsu/ccfit/muratov/distributed/crack/worker/service/PingService.java
+++ src/main/java/ru/nsu/ccfit/muratov/distributed/crack/worker/service/PingService.java
@@ -2,12 +2,14 @@ package ru.nsu.ccfit.muratov.distributed.crack.worker.service;
 
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.nsu.ccfit.muratov.distributed.crack.worker.dto.PortDto;
+import ru.nsu.ccfit.muratov.distributed.crack.worker.dto.WorkerIdDto;
 
 @Component
 @EnableScheduling
@@ -17,15 +19,24 @@ public class PingService {
     @Value("${server.port}")
     private int port;
 
+    private String id;
+
     @Scheduled(fixedRate = 3000)
     public void greetManager() {
         RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-        restTemplate.postForEntity(uriTemplate, new PortDto(port), PortDto.class);
+        ResponseEntity<WorkerIdDto> response = restTemplate.postForEntity(uriTemplate, new PortDto(port), WorkerIdDto.class);
+        WorkerIdDto dto = response.getBody();
+        if(dto != null) {
+            id = dto.getId();
+        }
     }
 
     @PreDestroy
     public void fareManager() {
+        if(id == null) {
+            return;
+        }
         RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-        restTemplate.delete(uriTemplate, new PortDto(port), PortDto.class);
+        restTemplate.delete(uriTemplate + "/" + id, new PortDto(port), PortDto.class);
     }
 }
